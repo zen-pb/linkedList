c7211695ca3e82b66d6d918a26b1c92bcaa9c463
Edizen Buguina <111935236+zen-pb@users.noreply.github.com>
2025-07-22 21:21:47 +0800

    Add initial structure of code

---
+++ LinkedList.js
@@ -0,0 +1,60 @@
+class LinkedList {
+    constructor(){
+        this.head = new Node();
+    }
+
+    append(value) {
+        
+    }
+
+    prepend(value){
+
+    }
+
+    size(){
+
+    }
+
+    head(){
+
+    }
+
+    tail(){
+
+    }
+
+    at(index){
+
+    }
+
+    pop(){
+
+    }
+
+    contains(value){
+
+    }
+
+    find(value){
+
+    }
+
+    toString(){
+
+    }
+
+    insertAt(value, index){
+
+    }
+
+    removeAt(index){
+        
+    }
+}
+
+class Node {
+    constructor(){
+        this.value = null;
+        this.nextNode = null;
+    }
+}
